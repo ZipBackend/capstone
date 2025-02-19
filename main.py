066714be2f6709595ed43f5dbd10fa770eb69159
Henry Dela Cruz <166102407+ZipBackend@users.noreply.github.com>
2025-02-19 16:13:28 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("THIS IS THE GREATEST SHOW")
+st.subheader("I close my eyes and i can't see")
+st.write("The world is waiting up for me")
+st.divider()
+st.image("Hello.png", "Masaya ka pa ba?")
